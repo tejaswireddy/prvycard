6d86b2c565126ccffadeb7c1e29c47cd590b3e60
tejaswireddy <tejaswireddy555@gmail.com>
2020-08-31 16:20:42 -0400

    index access issue fix

--- PrvyCard-Server/app.js
+++ PrvyCard-Server/app.js
@@ -102,7 +102,8 @@ app.set('port',8013)
 
 //bodyparser for posting the form related Data
 app.use(bodyParser.urlencoded({ extended : false}))
-app.set('views',__dirname+'/views');
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
   res.render('index');
